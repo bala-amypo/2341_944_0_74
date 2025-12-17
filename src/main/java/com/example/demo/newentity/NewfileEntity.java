ea4f83634015e13cfafbb4dde075c8554fb74bce
bala-amypo <bala@amypo.in>
2025-12-17 08:20:38 +0000

    User commit

--- src/main/java/com/example/demo/newentity/NewfileEntity.java
+++ src/main/java/com/example/demo/newentity/NewfileEntity.java
@@ -5,28 +5,32 @@ package com.example.demo.newentity;
   import jakarta.persistence.Id;
   import jakarta.validation.constranints.*;
   
- 
+ @entity
+ @Table(name="Student")
  public class NewfileEntity {
     @Id 
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     @NotBlank(message = "should not contain spaces")
     private String name;
     @column(name=unique)
      @NotBlank(message = "no blank allowed")
      @Email(message = "invalid format")
     private String email;
-    
-    public NewfileEntity(long id, @NotBlank(message = "should not contain spaces") String name,  
-     @NotBlank(message = "no blank allowed")  @Email(message = "invalid format") String email) {
+
+    public NewfileEntity(Long id, @NotBlank(message = "should not contain spaces") String name,  
+         @NotBlank(message = "no blank allowed")  @Email(message = "invalid format") String email) {
       this.id = id;
       this.name = name;
       this.email = email;
     }
-    public long getId() {
+    public NewfileEntity(){
+
+    }
+    public Long getId() {
        return id;
     }
-    public void setId(long id) {
+    public void setId(Long id) {
        this.id = id;
     }
     public String getName() {
