1bbc9274626b04ca36b33e7ee91bca50e80d88ca
bala-amypo <bala@amypo.in>
2025-12-17 05:13:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,1 +1,40 @@
- 
+ package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Project.entity.Studententity;
+import com.example.Project.service.Studentservice;
+
+@RestController
+public class Studentcontroller {
+   @Autowired
+   Studentservice src;
+   @PostMapping("/post")
+   public Studententity postdata(@RequestBody Studententity st){
+      return src.savedata(st);
+   }
+   @GetMapping("/get")
+   public List<Studententity> getdata(){
+      return src.retdata();
+   }
+   @GetMapping("/getid{id}")
+   public Studententity getIdVal(@PathVariable int id){
+         return src.id(id);
+   }
+   @PutMapping("/update/{id}")
+   public Studententity updateId(@PathVariable int id,@RequestBody Studententity st){
+      return src. update(id,st);
+   }
+   @DeleteMapping("/delete/{id}")
+   public Studententity deleteId(@PathVariable int id){
+      return src.delete(id);
+   }
+}
