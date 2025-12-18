3d54162863c77d875aab3fbcc9af6b3252bb778b
bala-amypo <bala@amypo.in>
2025-12-18 04:59:34 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,59 +1,59 @@
- package com.example.demo.entity;
+//  package com.example.demo.entity;
 
-import java.time.LocalDate;
+// import java.time.LocalDate;
 
-@entity
-public class StudentEntity {
-    @id
-    @GenerateValue(strategy = GenerationType.IDENTITY )
-    private Integer id;
-    @GeneratedValue(strategy = GenerationType.Auto)
-    private Integer roll;
-    private String name;
-    private String email;
-    private float cgpa;
+// @entity
+// public class StudentEntity {
+//     @id
+//     @GenerateValue(strategy = GenerationType.IDENTITY )
+//     private Integer id;
+//     @GeneratedValue(strategy = GenerationType.Auto)
+//     private Integer roll;
+//     private String name;
+//     private String email;
+//     private float cgpa;
 
-    public StudentEntity(String name, int id, LocalDate dob, String email, float cgpa) {
-        this.name = name;
-        this.id = id;
-        this.dob = dob;
-        this.email = email;
-        this.cgpa = cgpa;
-    }
+//     public StudentEntity(String name, int id, LocalDate dob, String email, float cgpa) {
+//         this.name = name;
+//         this.id = id;
+//         this.dob = dob;
+//         this.email = email;
+//         this.cgpa = cgpa;
+//     }
 
-    public StudentEntity(){
+//     public StudentEntity(){
 
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public int getId() {
-        return id;
-    }
+//     }
+//     public String getName() {
+//         return name;
+//     }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+//     public int getId() {
+//         return id;
+//     }
     
-    public void setId(int id) {
-        this.id = id;
-    }
-    public LocalDate getDob() {
-        return dob;
-    }
-    public void setDob(LocalDate dob) {
-        this.dob = dob;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public float getCgpa() {
-        return cgpa;
-    }
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
+//     public void setId(int id) {
+//         this.id = id;
+//     }
+//     public LocalDate getDob() {
+//         return dob;
+//     }
+//     public void setDob(LocalDate dob) {
+//         this.dob = dob;
+//     }
+//     public String getEmail() {
+//         return email;
+//     }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+//     public float getCgpa() {
+//         return cgpa;
+//     }
+//     public void setCgpa(float cgpa) {
+//         this.cgpa = cgpa;
+//     }
     
-}
+// }
--- src/main/java/com/example/demo/repository/StudentRepo.java
+++ src/main/java/com/example/demo/repository/StudentRepo.java
@@ -1,10 +1,10 @@
-package com.example.demo.repository;
+// package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.student;
-@Repository
-public interface  StudentRepo extends JpaRepository<Student,Long>{
+// import com.example.demo.entity.student;
+// @Repository
+// public interface  StudentRepo extends JpaRepository<Student,Long>{
 
-}
+// }
